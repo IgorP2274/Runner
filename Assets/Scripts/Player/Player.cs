814531b3c7a831ee3a24396b72ee296dab43d5b7
IgorP2274 <126153949+IgorP2274@users.noreply.github.com>
2023-11-27 22:27:30 +0300

    1.8

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -20,7 +20,7 @@ public class Player : MonoBehaviour
         _health -= damage;
         ChangeHealth?.Invoke(_health);
 
-        if (_health < 0) 
+        if (_health <= 0) 
             Die();
     }
 
